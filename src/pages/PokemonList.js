1a3c1250ed0f43070d39d2600b37870b13a63f7c
Aldo Wijaya <aldowjy12@gmail.com>
2021-06-22 14:52:19 +0700

    Add Button Load More

--- src/pages/PokemonList.js
+++ src/pages/PokemonList.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
 import CardContent from "@material-ui/core/CardContent";
 import Skeleton from "@material-ui/lab/Skeleton";
 import { gql, useQuery } from "@apollo/client";
@@ -35,22 +37,40 @@ const GET_POKEMONS = gql`
 `;
 
 const gqlVariables = {
-  limit: 50,
+  limit: 10,
   offset: 1,
 };
 
-export default function PokemonsTable() {
-  const { error, loading, data } = useQuery(GET_POKEMONS, {
+const defaultPokemons = {
+  status: true,
+  count: 0,
+  results: [],
+};
+
+const getLocalStorage = () => {
+  let data = [];
+
+  if (typeof localStorage !== "undefined") {
+    data = JSON.parse(localStorage.getItem("pokemon_history"));
+  } else {
+    data = [];
+  }
+
+  return data;
+};
+
+function PokemonList() {
+  const [pokemons, setPokemons] = useState(defaultPokemons);
+  const [page, setPage] = useState(0);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+  const myData = getLocalStorage();
+
+  const { loading, error, data } = useQuery(GET_POKEMONS, {
     variables: gqlVariables,
   });
 
-  let myData = [];
-  if (typeof localStorage !== "undefined") {
-    myData = JSON.parse(localStorage.getItem("pokemon_history"));
-  } else {
-    myData = [];
-  }
-
+  console.log("TEST", pokemons);
   if (loading)
     return (
       <Grid item xs={6}>
@@ -81,37 +101,48 @@ export default function PokemonsTable() {
   }
 
   return (
-    <Grid container spacing={3}>
-      {newData.map((pokemon) => (
-        <Grid item xs={12} sm={6} key={pokemon.name}>
-          <Link
-            to={`/detail/${pokemon.name}`}
-            style={{ textDecoration: "none" }}
-          >
-            <StyleCard>
-              <CardContent>
-                <Typography variant="caption" color="textSecondary">
-                  Pokemon
-                </Typography>
-                <Typography
-                  variant="h5"
-                  component="p"
-                  color="secondary"
-                  gutterBottom
-                >
-                  {pokemon.name}
-                </Typography>
-                <Typography variant="caption" color="textSecondary">
-                  Owned Total
-                </Typography>
-                <Typography variant="h6" component="p">
-                  {pokemon.own != null ? pokemon.own : "0"}
-                </Typography>
-              </CardContent>
-            </StyleCard>
-          </Link>
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid container spacing={3}>
+        {newData.map((pokemon) => (
+          <Grid item xs={12} sm={6} key={pokemon.name}>
+            <Link
+              to={`/detail/${pokemon.name}`}
+              style={{ textDecoration: "none" }}
+            >
+              <StyleCard>
+                <CardContent>
+                  <Typography variant="caption" color="textSecondary">
+                    Pokemon
+                  </Typography>
+                  <Typography
+                    variant="h5"
+                    component="p"
+                    color="secondary"
+                    gutterBottom
+                  >
+                    {pokemon.name}
+                  </Typography>
+                  <Typography variant="caption" color="textSecondary">
+                    Owned Total
+                  </Typography>
+                  <Typography variant="h6" component="p">
+                    {pokemon.own != null ? pokemon.own : "0"}
+                  </Typography>
+                </CardContent>
+              </StyleCard>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+      <Grid container justify="center">
+        <Box m={2}>
+          <Button variant="contained" color="secondary" disabled={loading}>
+            Load More
+          </Button>
+        </Box>
+      </Grid>
+    </>
   );
 }
+
+export default PokemonList;
